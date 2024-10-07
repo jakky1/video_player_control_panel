2d32b3ab841bb5e790b0d7fb9d0da2533fa90765
jakky1 <jakky1@gmail.com>
2024-10-07 10:50:56 +0800

    update folder example\android

--- example/android/app/src/main/java/jakky1/dev/video_player_control_panel_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package jakky1.dev.video_player_control_panel_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ example/android/app/src/main/kotlin/com/example/untitled1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.untitled1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
